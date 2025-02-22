8d8376d0c09273373e04138e8c40fd5abbf298a6
Jeremiah Egwa <jerriee232@gmail.com>
2025-02-22 11:52:48 +0100

    Innitialized advice app

---
+++ advice-app/src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+async function getAdvice () {
+
+}
+
+export default function App() {
+  return <div>
+    <h1>Hello World</h1>
+    <button>Get Advice</button>
+  </div>
+} 
