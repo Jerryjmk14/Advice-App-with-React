3879acbcd6c0ebd8ff3113c79e85bd97921c9d3c
Jeremiah Egwa <jerriee232@gmail.com>
2025-02-22 12:57:29 +0100

    completed a simple advice app

--- advice-app/src/App.jsx
+++ advice-app/src/App.jsx
@@ -1,13 +1,29 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-
-async function getAdvice () {
-
-}
+import React, { useState, useEffect } from 'react'
 
 export default function App() {
-  return <div>
-    <h1>Hello World</h1>
-    <button>Get Advice</button>
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice () {
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const data = await response.json()
+    setAdvice(data.slip.advice);
+    setCount((count => count + 1))
+  }
+
+  useEffect(() => {
+    getAdvice()
+  }, [])
+  
+  return <div className='display'>
+    <Title/>
+    <h2>{advice}</h2>
+    <button onClick={getAdvice}>Get Advice</button>
+    <p>You have read <strong>{count}</strong> pieces of advice.</p>
   </div>
 } 
+
+ function Title() {
+  return <h1>Advice App</h1>
+}
